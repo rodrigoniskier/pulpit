d396aa89fc69014891bf8a535acb9175bd865b60
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-09-07 19:21:28 -0300

    chore: cache producer feedback features offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pulpit-ultimate-v2.0.3';
-const APP_SHELL=['./','./index.html','./styles.css','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./src/app.js','./src/db.js','./src/schema.js','./src/utils.js','./src/markdown.js','./src/mirror.js','./src/backup.js','./src/bible.js','./src/prompt.js'];
+const CACHE='pulpit-ultimate-v2.2.0';
+const APP_SHELL=['./','./index.html','./styles.css','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./src/app.js','./src/db.js','./src/schema.js','./src/utils.js','./src/markdown.js','./src/mirror.js','./src/backup.js','./src/bible.js','./src/bible-selector.js','./src/producer-feedback.js','./src/prompt.js'];
 const YOUVERSION_ORIGIN='https://api.youversion.com';
 
 self.addEventListener('install',event=>{
